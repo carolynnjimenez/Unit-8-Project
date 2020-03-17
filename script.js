33f2407c8300b2f84de5991f833d33d320b755d5
carolynnjimenez <40403222+carolynnjimenez@users.noreply.github.com>
2020-03-17 11:33:07 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ $("button").click(function() {
 
     if (age > 18 && gender === "girl") {
         $(".result").append(`<p>You are Caro!<p>`);
+        $(".result").append(`<img src="caro.JPG">`);
+
     } else if (age <= 18 && gender === "girl") {
         $(".result").append(`<p>You are Geno!<p>`);
+        $(".result").append(`<img src="geno.jpg">`);
     }  else if ( age > 18 && gender === "boy"){
         $(".result").append(`<p>You are David!<p>`);
+        $(".result").append(`<img src="david.jpg">`);
     }   else if ( age <= 18 && gender === "boy"){
         $(".result").append(`<p>You are Luis!<p>`);
+        $(".result").append(`<img src="luis.gif">`);
+
     } else {
         $(".result").append(`<p>Please try again<p>`);
 
